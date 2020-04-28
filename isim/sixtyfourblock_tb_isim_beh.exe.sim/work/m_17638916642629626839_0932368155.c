f8b53c7570719ee0683829046076f9fe4d1ec41d
AldenHunt <43453409+AldenHunt@users.noreply.github.com>
2020-04-27 22:08:31 -0400

    recompiled

--- isim/sixtyfourblock_tb_isim_beh.exe.sim/work/m_17638916642629626839_0932368155.c
+++ isim/sixtyfourblock_tb_isim_beh.exe.sim/work/m_17638916642629626839_0932368155.c
@@ -21,11 +21,11 @@
 #include <malloc.h>
 #define alloca _alloca
 #endif
-static const char *ng0 = "/home/ise/ise_projects/Thesis/clockedadd.v";
+static const char *ng0 = "/home/ise/ise_projects/Thesis/fourblock.v";
 
 
 
-static void Always_30_0(char *t0)
+static void Always_51_0(char *t0)
 {
     char t7[8];
     char *t1;
@@ -35,40 +35,40 @@ static void Always_30_0(char *t0)
     char *t5;
     char *t6;
 
-LAB0:    t1 = (t0 + 2680U);
+LAB0:    t1 = (t0 + 3320U);
     t2 = *((char **)t1);
     if (t2 == 0)
         goto LAB2;
 
 LAB3:    goto *t2;
 
-LAB2:    xsi_set_current_line(30, ng0);
-    t2 = (t0 + 3000);
+LAB2:    xsi_set_current_line(51, ng0);
+    t2 = (t0 + 3640);
     *((int *)t2) = 1;
-    t3 = (t0 + 2712);
+    t3 = (t0 + 3352);
     *((char **)t3) = t2;
     *((char **)t1) = &&LAB4;
 
 LAB1:    return;
-LAB4:    xsi_set_current_line(30, ng0);
+LAB4:    xsi_set_current_line(51, ng0);
 
-LAB5:    xsi_set_current_line(31, ng0);
-    t4 = (t0 + 1048U);
+LAB5:    xsi_set_current_line(52, ng0);
+    t4 = (t0 + 1848U);
     t5 = *((char **)t4);
-    t4 = (t0 + 1208U);
+    t4 = (t0 + 2008U);
     t6 = *((char **)t4);
     memset(t7, 0, 8);
     xsi_vlog_signed_add(t7, 16, t5, 16, t6, 16);
-    t4 = (t0 + 1768);
-    xsi_vlogvar_wait_assign_value(t4, t7, 0, 0, 16, 0LL);
+    t4 = (t0 + 2408);
+    xsi_vlogvar_assign_value(t4, t7, 0, 0, 16);
     goto LAB2;
 
 }
 
 
-extern void work_m_13547184716116890591_2714661984_init()
+extern void work_m_17638916642629626839_0932368155_init()
 {
-	static char *pe[] = {(void *)Always_30_0};
-	xsi_register_didat("work_m_13547184716116890591_2714661984", "isim/sixtyfourblock_tb_isim_beh.exe.sim/work/m_13547184716116890591_2714661984.didat");
+	static char *pe[] = {(void *)Always_51_0};
+	xsi_register_didat("work_m_17638916642629626839_0932368155", "isim/sixtyfourblock_tb_isim_beh.exe.sim/work/m_17638916642629626839_0932368155.didat");
 	xsi_register_executes(pe);
 }
